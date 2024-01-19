ae7e4e779a81a3a4dac2a5c12862d001f258d120
miguel <inventado@gmail.com>
2024-01-19 20:41:27 +0100

    mas

--- src/main/java/proyectoFinal/Principal.java
+++ src/main/java/proyectoFinal/Principal.java
@@ -25,9 +25,9 @@ public class Principal {
 		
 		
 		User u1 = new User("user","clave","nombre","apellidos","dni","V","user@dominio.es",
-				916050000,new Date(),Arrays.asList(admin));
+				916050000,new Date(),admin);
 		User u2 = new User("user2","clave2","nombre2","apellidos2","dni2","V","user2@dominio.es",
-				916050002,new Date(),Arrays.asList(oper));
+				916050002,new Date(),oper);
 		
 		udao.putUser(u1);
 		udao.putUser(u2);
